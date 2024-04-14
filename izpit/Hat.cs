483a97facd82eaad809b4f4d8d346db719e47041
Borko <107365623+Borko1234@users.noreply.github.com>
2024-04-14 10:19:06 +0300

    Add files via upload

---
+++ izpit/Hat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatExam
+{
+    public class Hat
+    {
+        private string type;
+        private string color;
+        private double price;
+
+        public Hat(string type, string color, double price)
+        {
+            this.Type = type;
+            this.Color = color;
+            this.Price = price;
+        }
+
+        public string Type
+        {
+            get { return this.type; }
+            set { this.type = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public double Price
+        {
+            get { return this.price; }
+            set {
+                if(value > 100)
+                {
+                    throw new ArgumentException("Invalid hat price!");
+                }
+                this.price = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Hat {this.type} with color {this.color} costs {this.price:0.00}";
+        }
+    }
+}
---
+++ izpit/HatShop.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatExam
+{
+    public class HatShop
+    {
+        private string name;
+        private List<Hat> hats;
+
+        public HatShop(string name)
+        {
+            this.hats = new List<Hat>();
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { 
+                if(value.Length<6)
+                {
+                    throw new ArgumentException("Invalid hat shop name!");
+                }
+                this.name = value; 
+            }
+        }
+
+        public void AddHat(Hat hat)
+        {
+            hats.Add(hat);
+        }
+
+        public bool SellHat(Hat hat)
+        {
+            bool contains = false;
+            foreach (var item in hats)
+            {
+                contains = (hat.Type == item.Type
+                    && hat.Color == item.Color
+                    && hat.Price == item.Price);
+                //if(hats.Contains(hat))
+                if (contains)
+                {
+                    hats.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+
+        public double CalculateTotalPrice()
+        {
+            return hats.Sum(x => x.Price);
+        }
+
+        public Hat GetHatWithHighestPrice()
+        {
+            return hats.OrderByDescending(x => x.Price).ToList()[0];
+        }
+
+        public Hat GetHatWithLowestPrice()
+        {
+            return hats.OrderBy(x => x.Price).ToList()[0];
+        }
+
+        public void RenameHatShop(string newName)
+        {
+            this.Name = newName;
+        }
+
+        public void SellAllHats()
+        {
+            hats.Clear();
+        }
+
+        public override string ToString()
+        {
+            int hatCount = hats.Count;
+            if(hatCount > 0)
+            {
+                return $"Hat Shop {this.name} has {hatCount} hat/s:\n" + String.Join("\n",
+                    hats.Select(x => x.ToString()));
+            }
+            else
+            {
+                return $"Hat Shop {this.name} has no available hats.";
+            }
+        }
+    }
+}
