d65b6241529165d36fe7538a8986b844f20a0139
Borko <107365623+Borko1234@users.noreply.github.com>
2024-09-28 12:44:32 +0300

    Add files via upload

---
+++ zadacha5.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Car");
+            Truck truck = new Truck("Truck");
+            MachineOperator mo = new MachineOperator(car);
+            Console.WriteLine(mo.Start());  //пускаме машината
+            Console.WriteLine(mo.Stop()); //спираме машината
+            mo = new MachineOperator(truck);
+            Console.WriteLine(mo.Start()); 
+            Console.WriteLine(mo.Stop());
+        }
+    }
+}
