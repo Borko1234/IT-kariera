245a2ab8c0c069b9934f557bb66555857e452cdf
Borko <107365623+Borko1234@users.noreply.github.com>
2024-09-29 10:12:58 +0300

    Add files via upload

---
+++ zadacha 5.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_5._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cat cat = new cat();
+            Trainer mo= new Trainer(cat);
+            mo.Make();
+
+        }
+    }
+}
---
+++ zadacha 5.4/Trainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_5._4
+{
+    internal class Trainer
+    {
+		private IAnimal entity;
+        private cat cat;
+
+        public  IAnimal Entity
+		{
+			get { return entity; }
+			set { entity = value; }
+		}
+		public Trainer(IAnimal entity)
+		{
+			Entity = entity;
+		}
+		public void Make()
+		{
+            Console.WriteLine(this.Entity.Perform());
+        }
+
+
+
+
+    }
+}
