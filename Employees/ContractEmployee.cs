019614bde1e824574f816903f1cdce7fc5b9c38a
Borko <107365623+Borko1234@users.noreply.github.com>
2024-10-05 11:57:02 +0300

    Add files via upload

---
+++ Employees/ContractEmployee.cs
@@ -0,0 +1,38 @@
+﻿namespace Employees
+{
+    class ContractEmployee : BaseEmployee
+    {
+        private string employeeTask;
+        private string employeeDepartment;
+
+        public ContractEmployee
+        (
+            string employeeID, 
+            string employeeName, 
+            string employeeAddress, 
+            string employeeTask,
+            string employeeDepartment
+        ) : base(employeeID, employeeName, employeeAddress)
+        {
+            this.employeeTask = employeeTask;
+            this.employeeDepartment = employeeDepartment;
+        }
+
+        public void Show()
+        {
+            base.Show();
+            Console.WriteLine("Employee task: " + this.employeeTask);
+            Console.WriteLine("Employee department: " + this.employeeDepartment);
+        }
+
+        public override double CalculateSalary(int workingHours)
+        {
+            return 250 + workingHours * 20;
+        }
+
+        public override string GetDepartment()
+        {
+            return this.employeeDepartment;
+        }
+    }
+}
---
+++ Employees/FullTimeEmployee.cs
@@ -0,0 +1,38 @@
+﻿namespace Employees
+{
+    public class  FullTimeEmployee : BaseEmployee
+    {
+        private string employeePosition;
+        private string employeeDepartment;
+
+        public FullTimeEmployee
+        (
+            string employeeID, 
+            string employeeName, 
+            string employeeAddress, 
+            string employeePosition,
+            string employeeDepartment
+        ) : base(employeeID, employeeName, employeeAddress)
+        {
+            this.employeePosition = employeePosition;
+            this.employeeDepartment = employeeDepartment;
+        }
+
+        public void Show()
+        {
+            base.Show();
+            Console.WriteLine("Employee position: " + this.employeePosition);
+            Console.WriteLine("Employee department: " + this.employeeDepartment);
+        }
+
+        public override double CalculateSalary(int workingHours)
+        {
+            return 250 + workingHours*10.80;
+        }
+
+        public override string GetDepartment()
+        {
+            return this.employeeDepartment;
+        }
+    }
+}
