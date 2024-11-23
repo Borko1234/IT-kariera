872e6ce0c85099cd99675cda86d89b56b4b1a76c
Borko <107365623+Borko1234@users.noreply.github.com>
2024-11-23 14:45:58 +0200

    Update Suzdavane_na_tablici.sql

--- SQL/Suzdavane_na_tablici.sql
+++ SQL/Suzdavane_na_tablici.sql
@@ -3,9 +3,9 @@ use minions;
 CREATE TABLE minions
 (
 Id INT AUTO_INCREMENT,
-name VARCHAR(50) NOT NULL,
+name VARCHAR(50) NOT NULL,    //varchar znachi che moje da promenqsh kolko da e dulug
 age INT NOT NULL,
-PRIMARY KEY(Id)
+PRIMARY KEY(Id)               //neshtoto po koeto gi razlichavash i ne moje da se povtarq
 );
 INSERT INTO minions(name,age) VALUES('gosho',10);
 INSERT INTO minions(name,age) VALUES('misho',14);
@@ -16,8 +16,8 @@ INSERT INTO minions(name,age) VALUES('aleks',4);
 CREATE TABLE town
 (
 Id INT AUTO_INCREMENT,
-name VARCHAR(50) NOT NULL,
-PRIMARY KEY(Id)
+name VARCHAR(50) NOT NULL,     //varchar znachi che moje da promenqsh kolko da e dulug
+PRIMARY KEY(Id)                //neshtoto po koeto gi razlichavash i ne moje da se povtarq
 );
 INSERT INTO town (name) VALUES ('SigmaTown');
 INSERT INTO town (name) VALUES ('SkibidiTown');
